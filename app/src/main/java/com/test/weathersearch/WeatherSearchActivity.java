ebdae56d2de4188e31e5d81bb55545202b8d6a04
Anh Vu <anhvt52@gmail.com>
2017-10-20 14:12:07 +0700

    first commit

---
+++ app/src/main/java/com/test/weathersearch/WeatherSearchActivity.java
@@ -0,0 +1,13 @@
+package com.test.weathersearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_search);
+    }
+}
